a1a449523c3c1802a9b93339eaaaa92950659236
Abhay Ganti <abhayganti0909@gmail.com>
2022-11-14 06:31:59 +0800

    Added scraper and QA model

--- model.py
+++ model.py
@@ -2,16 +2,19 @@ from transformers import AutoTokenizer, AutoModelForQuestionAnswering
 from transformers import pipeline
 from scraper import get_html
 
+# Init variables from scraper.py
 url = 'https://scikit-learn.org/stable/modules/sgd.html'
 context = get_html(url)
 
+# Load model and tokenizer
 model = AutoModelForQuestionAnswering.from_pretrained("deepset/roberta-base-squad2")
 
-question = 'What is the shape of the X array?'
+question = str(input('What is your question: '))
 
 tokenizer = AutoTokenizer.from_pretrained("deepset/roberta-base-squad2")
 tokenizer.encode(question, truncation=True, padding=True)
 
+# Creating a pipeline for QA
 nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
 
 print(nlp({
--- scraper.py
+++ scraper.py
@@ -1,12 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+# Scraping HTML from a single Sklearn docs page
 def get_html(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    for data in soup([['style', 'script']]):
+    # Get the main content of the page
+    for data in soup(['style', 'script']):
         data.decompose()
 
-    text= ' '.join(soup.stripped_strings).replace('\n', ' ')
+    text = ' '.join(soup.stripped_strings).replace('\n', ' ')
     return text
